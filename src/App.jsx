6c488041b32399ff01f819085b2607225ca5b7eb
MrximFant <Fanter@gmail.com>
2026-04-17 00:57:19 +0200

    f

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>958 Planner</h1>
+      <p>Welcome to your React application!</p>
+    </div>
+  )
+}
+
+export default App
